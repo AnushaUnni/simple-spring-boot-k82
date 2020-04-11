55157013453ee010060c3f45e026ce21b0077c49
Anusha Unni <Anusha.Vallapil@tatacomunications.com>
2020-04-11 20:49:39 +0530

    Deployement scripts of k8s

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+docker build -t anushaunni/springboot:latest -t anushaunni/springboot:$SHA -f Dockerfile .
+docker push anushaunni/springboot:latest
+docker push anushaunni/springboot:$SHA
+kubectl apply -f k8s
+kubectl set image deployments/simple-controller-deployment simple-controller=anushaunni/springboot:$SHA
