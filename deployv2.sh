142ad3b67c6ca30cca803788467b48072453c3fc
Anusha Unni <Anusha.Vallapil@tatacomunications.com>
2020-04-12 16:30:23 +0530

    build in travis yml

--- deployv2.sh
+++ deployv2.sh
@@ -1,7 +1,4 @@
 pwd
 ls -lrt
-docker build -t anushaunni/springboot:latest -t anushaunni/springboot:$SHA .
-docker push anushaunni/springboot:latest
-docker push anushaunni/springboot:$SHA
 kubectl apply -f k8s
 kubectl set image deployments/simple-controller-deployment simple-controller=anushaunni/springboot:$SHA
